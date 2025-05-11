892d9b1bac6ac7e503b2b9ebf058d3f57e8f19bc
TheMrNewVegas <112358568+TheMrNewVegas@users.noreply.github.com>
2025-05-11 04:52:00 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="docs/home">
-            Home Page
+            About Us
           </Link>
         </div>
       </div>
