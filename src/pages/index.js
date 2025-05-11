d9cae56a163491628531ae92e2400081f36636f4
TheMrNewVegas <112358568+TheMrNewVegas@users.noreply.github.com>
2025-05-11 04:51:50 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,11 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="docs/home">
+            Home Page
+          </Link>
         </div>
       </div>
     </header>
